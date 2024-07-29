777a81b929486b41020dea8b86853eda994e1822
ArthurSousa460 <arthurprogrammer460@gmail.com>
2024-07-28 22:38:35 -0300

    up app.js

--- src/app.js
+++ src/app.js
@@ -5,14 +5,15 @@ import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || '0.0.0.0';
 
 app.use(cors());
 router(app);
 
 
-app.listen(PORT, (err) =>{
+app.listen(PORT,HOST, (err) =>{
     if(err){
         console.log(err);
     }
-    console.log("Server running http://127.0.0.1:8080")
+    console.log(`Server running http://${HOST}:8080`)
 })
